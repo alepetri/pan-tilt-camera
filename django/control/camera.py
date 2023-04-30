08d201c246b9ddef5a405172b513785b142f827e
Andy Lepetri <lepetriandy@gmail.com>
2023-04-30 12:58:37 -0700

    Added camera digital zoom functionality and ui

--- django/control/camera.py
+++ django/control/camera.py
@@ -8,10 +8,18 @@ import threading
 from picamera2 import Picamera2
 from libcamera import Transform, controls # type: ignore
 
+from enum import Enum
+
 class Camera:
+    class ZoomState(Enum):
+        NONE = 1
+        ZOOM_IN = 2
+        ZOOM_OUT = 3
+
     thread = None  # background thread that reads frames from camera
     frame = None  # current frame is stored here by background thread
     last_access = 0  # time of last client access to the camera
+    zoom_state: ZoomState = ZoomState.NONE
 
     def initialize(self):
         if Camera.thread is None:
@@ -28,6 +36,12 @@ class Camera:
         self.initialize()
         return self.frame
 
+    def zoom(self, zoom_cmd: ZoomState):
+        Camera.last_access = time.time()
+        self.initialize()
+        if Camera.zoom_state == Camera.ZoomState.NONE:
+            Camera.zoom_state = zoom_cmd
+
     @classmethod
     def _thread(cls):
         print("Starting Camera!")
@@ -42,8 +56,26 @@ class Camera:
             # let camera warm up
             time.sleep(2)
 
+            # https://github.com/raspberrypi/picamera2/blob/main/examples/zoom.py
+            size_px = camera.capture_metadata()['ScalerCrop'][2:4] # type: ignore
+            full_size_px = camera.camera_properties['PixelArraySize']
+
             data = io.BytesIO()
             while time.time() - cls.last_access < 10:
+                camera.capture_metadata()
+
+                if cls.zoom_state == cls.ZoomState.ZOOM_IN:
+                    size_px = [int(s * 0.95) for s in size_px]
+                elif cls.zoom_state == cls.ZoomState.ZOOM_OUT:
+                    size_px = [int(s * 1.05) for s in size_px]
+                
+                if cls.zoom_state != cls.ZoomState.NONE:
+                    if any(s > r for r, s in zip(full_size_px, size_px)):
+                        size_px = full_size_px.deepcopy()
+                    offset_px = [(r - s) // 2 for r, s in zip(full_size_px, size_px)]
+                    camera.set_controls({'ScalerCrop': offset_px + size_px})
+                    cls.zoom_state = cls.ZoomState.NONE
+
                 # store frame
                 camera.capture_file(data, format='jpeg')
 
